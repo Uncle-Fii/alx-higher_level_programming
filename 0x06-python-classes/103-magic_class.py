3fcd1f21b7078f6d6763d11e00e373da134aa0db
Uncle-Fii <fkankrumah@gmail.com>
2022-06-21 16:25:31 -0700

    edits file 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""Magic class from a given ByteCode."""
+"""Class MagicClass"""
 import math
 
 
 class MagicClass:
-        """Initialization of the MagicClass."""
-            def __init__(self, radius=0):
-                        """Initialization of the data."""
-                                self._MagicClass__radius = 0
-                                        if type(radius) is not int and type(radius) is not float:
-                                                        raise TypeError("radius must be a number")
-                                                            self._MagicClass__radius = radius
+    """Defines MagicClass"""
+    def __init__(self, radius=0):
+        """Initializes Data"""
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
 
-                                                                def area(self):
-                                                                            """Calculation of the area."""
-                                                                                    return self._MagicClass__radius ** 2 * math.pi
+    def area(self):
+        """Get area"""
+        return (self.__radius ** 2) * math.pi
 
-                                                                                    def circumference(self):
-                                                                                                """Calculation of the circumference."""
-                                                                                                        return 2 * math.pi * self._MagicClass__radius
+    def circumference(self):
+        """Get Circumference"""
+        return (2 * math.pi * self.__radius)
