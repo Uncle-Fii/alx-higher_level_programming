f42e95be4f9c09d41c219883f147aca2cc6f2a76
Uncle-Fii <fkankrumah@gmail.com>
2022-06-21 14:51:54 -0700

    edits file 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-class Square:
-        """Represents a square.
-            Private instance attribute: size.
-                Instantiation with optional size.
-                    """
+"""Create a Class Square with size"""
 
-                        def __init__(self, size=0):
-                                """Initializes the data."""
-                                        if not isinstance(size, int):
-                                                    raise TypeError("size must be an integer")
-                                                            elif size < 0:
-                                                                        raise ValueError("size must be >= 0")
-                                                                            self.__size = size
+
+class Square:
+    """Class - Square"""
+
+    def __init__(self, size=0):
+        """Constructor of a Square with the size"""
+        if (type(size) is not int):
+            raise (TypeError("size must be an integer"))
+        elif (size < 0):
+            raise (ValueError("size must be >= 0"))
+        else:
+            self.__size = size
