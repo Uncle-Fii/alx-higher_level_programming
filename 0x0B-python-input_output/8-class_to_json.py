3f98d0462709e6855805e3a36d072c45a46ecac2
Uncle-Fii <fkankrumah@gmail.com>
2022-07-05 17:03:38 -0700

    edits file 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Defines a Python class-to-JSON function."""
+'''task 8 module'''
 
 
 def class_to_json(obj):
-    """Return the dictionary represntation of a simple data structure."""
-    return obj.
+    '''returns dict discription with simple data structure'''
+    return (obj.__dict__)
